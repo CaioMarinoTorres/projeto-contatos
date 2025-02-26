7e43ee3ca1d21e50ad9834d46fd2a08f79cf027d
Caio Marino <caiomt000@gmail.com>
2025-02-25 22:47:44 -0400

    att

---
+++ src/main/java/com/projetocontatos/ContatosControle.java
@@ -0,0 +1,15 @@
+package com.projetocontatos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class ContatosControle {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
